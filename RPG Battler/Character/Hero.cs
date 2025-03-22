21fa48c8db20ebdb908f7abf801433d34e307811
jcardona27 <juancardona696@gmail.com>
2025-03-21 22:11:35 -0600

    DONE

--- RPG Battler/Character/Hero.cs
+++ RPG Battler/Character/Hero.cs
@@ -21,11 +21,58 @@ namespace RPG_Battler.Character
         public List<Equipment> Equipment { get; set; }
 
         public Hero()
-        {       
+        {
+            Name = "Unknown";
+            Level = 0;
+            Health = 1;
+            Power = 1;
+            Luck = 1;
+            Mana = 0;
+            ExperienceRemaining = 100;
+            Items = new List<Item>();
+            Skills = new List<Skill>();
+            Spells = new List<Spell>();
+            Equipment = new List<Equipment>();
         }
 
         public void LevelUp()
         {
+            Random rand = new Random();
+            Level++;
+
+            switch (CombatClass.ToString())
+            {
+                case "Mage":
+                    Health += rand.Next(1, 20);
+                    Power += rand.Next(1, 10);
+                    Luck += rand.Next(1, 10);
+                    break;
+                case "Warrior":
+                    Health += rand.Next(10, 30);
+                    Power += rand.Next(1, 10);
+                    Luck += rand.Next(1, 10);
+                    break;
+                case "Rogue":
+                    Health += rand.Next(1, 25);
+                    Power += rand.Next(1, 10);
+                    Luck += rand.Next(1, 10);
+                    break;
+            }
+        }
+
+        public void DisplayStats(bool showTotalStats = true)
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Level: {Level}");
+            Console.WriteLine($"Health: {Health}");
+            Console.WriteLine($"Power: {Power}");
+            Console.WriteLine($"Luck: {Luck}");
+            Console.WriteLine($"Mana: {Mana}");
+
+            if (!showTotalStats)
+            {
+                Console.WriteLine("(Natural stats only - equipment stats not shown)");
+            }
         }
 
         public void CalculateTotals()
